37eb3eb3b3dba7ef8c4666eaed9f66319fed53b5
alisson539 <alissonramos539@gmail.com>
2020-06-10 06:19:32 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+//Select element function
+
+const selectElement = function (element) {
+   return document.querySelector(element);
+};
+
+let menuToggler = selectElement('.menu-toggle');
+let body = selectElement('body');
+
+
+menuToggler.addEventListener('click', function() {
+   body.classList.toggle('open');
+});
