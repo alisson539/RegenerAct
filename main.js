a1f9c4ecd0521afbde27df8f286c4508c7b537a4
alisson539 <alissonramos539@gmail.com>
2020-06-14 23:50:17 -0300

    New about not ready

--- main.js
+++ main.js
@@ -11,3 +11,12 @@ let body = selectElement('body');
 menuToggler.addEventListener('click', function() {
    body.classList.toggle('open');
 });
+
+//Scroll paralax
+
+window.addEventListener ('scroll', function() {
+   const parallax = document.querySelector('.parallax');
+   let scrollPosition = window.pageYOffset;
+   
+   parallax.style.transform = 'translateY(' + scrollPosition * .5 + 'px)';
+});
